cf694a5355ff03947d08a833e32c2b9344557e88
Garrod Alwood <galwood@smartronix.com>
2023-12-11 10:23:08 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+from qbittorrentapi import Client
+import subprocess
+
+# qBittorrent web UI details
+conn_info = dict(
+   host='<host ip or host>',
+   port='<qbit port>',
+   username='<qbit username>',
+   password='<qbit password>'
+)
+# Default is 'adminadmin', change if you've set a different one
+qbt_client = Client(**conn_info)
+# Desired port
+#NEW_PORT = 6881
+output = subprocess.check_output(['piactl', 'get', 'portforward'])
+NEW_PORT = output.decode('utf-8').strip()
+print(NEW_PORT)
+
+# Login to qBittorrent
+preferences_data = {
+    'listen_port': NEW_PORT
+}
+
+
+get_pref = qbt_client.app_preferences()
+if get_pref != NEW_PORT:
+    qbt_client.app_set_preferences(prefs=preferences_data)
+else:
+    print("same port")
